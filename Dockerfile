ba6cf2d50001c8ff5a8029ac26c59e1e9881a5cb
Togay Tunca <tuncatogay@gmail.com>
2024-02-19 17:40:11 +0300

    update2

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# which version of python
+FROM python
+
+# what code and docs
+
+
+
+# create a venv
+
+
+# run the app
--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
+
+import os
 from fastapi import FastAPI
+from src.env import config
+
+MODE = config("MODE", cast=str, default='test')
 
 app = FastAPI()
-
 
 @app.get("/")
 def home_page():
     # for API services
     # JSON-ready dict -> json.dumps({'hello': 'world'})
-    return {"Hello": "World"}
+    return {"Hello": "World", "Mode": MODE}
+
+
+
+
+
+## uvicorn src.main:app
